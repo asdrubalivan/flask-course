cb1a458828d242e3a9cc8708cf8ea06a401a5734
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-08-30 17:53:34 -0300

    Adding constants

--- resources/item.py
+++ resources/item.py
@@ -5,27 +5,33 @@ from flask_jwt_extended import (
 )
 from models.item import ItemModel
 
+BLANK_ERROR = "'{}' cannot be left blank!"
+ITEM_NOT_FOUND = "Item not found"
+ITEM_DELETED = "Item deleted"
+ITEM_ALREADY_EXISTS = "An item with name '{}' already exists."
+ITEM_ERROR_INSERT = "An error occurred while inserting the item."
+
 
 class Item(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument(
-        "price", type=float, required=True, help="This field cannot be left blank!"
+        "price", type=float, required=True, help=BLANK_ERROR.format("price")
     )
     parser.add_argument(
-        "store_id", type=int, required=True, help="Every item needs a store_id."
+        "store_id", type=int, required=True, help=BLANK_ERROR.format("store_id")
     )
 
     def get(self, name: str):
         item = ItemModel.find_by_name(name)
         if item:
             return item.json(), 200
-        return {"message": "Item not found."}, 404
+        return {"message": ITEM_NOT_FOUND}, 404
 
     @fresh_jwt_required
     def post(self, name: str):
         if ItemModel.find_by_name(name):
             return (
-                {"message": "An item with name '{}' already exists.".format(name)},
+                {"message": ITEM_ALREADY_EXISTS.format(name)},
                 400,
             )
 
@@ -36,7 +42,7 @@ class Item(Resource):
         try:
             item.save_to_db()
         except:
-            return {"message": "An error occurred while inserting the item."}, 500
+            return {"message": ITEM_ERROR_INSERT}, 500
 
         return item.json(), 201
 
@@ -45,8 +51,8 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
-            return {"message": "Item deleted."}, 200
-        return {"message": "Item not found."}, 404
+            return {"message": ITEM_DELETED}, 200
+        return {"message": ITEM_NOT_FOUND}, 404
 
     def put(self, name: str):
         data = Item.parser.parse_args()
